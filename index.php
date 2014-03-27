2385f6c05163e25f0ef945c1ba7d4a7d2c698088
gluckzu <gluckzu@gmail.com>
2014-03-27 12:53:51 +0100

    heders

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Photo Gallery</title>
+    <meta http-equiv="content-type" content="=text/html; charset=utf-8 without BOM"/>
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="stylesheet" href="style.css">
+</head>
+<!-------------------------------------------------------------------------------------->
+<body>
+	<?php
 
-echo"<h1>Prosjekt del 3 - Database</h1>";
+		echo"<h1>Prosjekt del 3 - Database</h1>";
 
-echo"<p>Spørringer: Lag liste over alle eiendeler</p><br>
-Spørringer: Lag liste over eiendeler som organisasjon har lånt med id, beskrivelse, eier, og leveringsdato</p><br>;
-Spørringer: Finn ut hvem som har lånt en bestemt eiendel. List opp navn og kontaktinformasjon</p><br>
-Spørringer: Finn ut hvem man må kontakte for å få tak i en bestemt eiendel/hvem som har adgang til plasseringen eiendelen ligger på</p><br>
-Spørringer: Lag liste over alle eiendeler</p><br><br>
+		echo"<p>Spørringer: Lag liste over alle eiendeler</p><br>
+			Spørringer: Lag liste over eiendeler som organisasjon har lånt med id, beskrivelse, eier, og leveringsdato</p><br>;
+			Spørringer: Finn ut hvem som har lånt en bestemt eiendel. List opp navn og kontaktinformasjon</p><br>
+			Spørringer: Finn ut hvem man må kontakte for å få tak i en bestemt eiendel/hvem som har adgang til plasseringen eiendelen ligger på</p><br>
+			Spørringer: Lag liste over alle eiendeler</p><br><br>
 
-Oppdateringer: <br>
-- Legg inn eiendeler med alle attributter, for alle fremmednøkler skal programmet vise frem en liste, GUI-pull-down meny over alternativer i stedet for et vanlig tekstfelt<br>
-- Legg inn nye medlemmer eiere og plasseringer";
+			Oppdateringer: <br>
+			- Legg inn eiendeler med alle attributter, for alle fremmednøkler skal programmet vise frem en liste, GUI-pull-down meny over alternativer i stedet for et vanlig tekstfelt<br>
+			- Legg inn nye medlemmer eiere og plasseringer";
 
-?>
+	?>
+</body>
+</html>
