d0ee3f70f11515e05c7fa1a2e58715855f11a4be
gluckzu <gluckzu@gmail.com>
2014-03-27 13:17:04 +0100

    db

--- index.php
+++ index.php
@@ -8,20 +8,14 @@
 </head>
 <!-------------------------------------------------------------------------------------->
 <body>
+	<a href="Oppgaver.html">Oppgaver</a>
 	<?php
-
-echo"<h1>Prosjekt del 3 - Database</h1><br>";
-
-echo"<h3>Spørringer:</h3>
-- Lag liste over alle eiendeler<br><br>
-- Lag liste over eiendeler som organisasjon har lånt med id, beskrivelse, eier, og leveringsdato<br><br>
-- Finn ut hvem som har lånt en bestemt eiendel. List opp navn og kontaktinformasjon<br><br>
-- Finn ut hvem man må kontakte for å få tak i en bestemt eiendel/hvem som har adgang til plasseringen eiendelen ligger på<br><br>
-- Lag liste over alle eiendeler<br><br><br>
-
-<h3>Oppdateringer: </h3>
-- Legg inn eiendeler med alle attributter, for alle fremmednøkler skal programmet vise frem en liste, GUI-pull-down meny over alternativer i stedet for et vanlig tekstfelt<br><br>
-- Legg inn nye medlemmer eiere og plasseringer";
+		include_once("mysql.php");
+		db_connnect();
+		// $query = "UPDATE file_liste SET rating='$rate' WHERE filename='$fileName'";
+		// $result = $db->query($query);
+		
+		
 
 	?>
 </body>
---
+++ mysql.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gluck
+ * Date: 07.02.14
+ * Time: 11:12
+ */
+
+$db_is_connected = false;
+// include_once('funksjoner.php');
+
+//  Connection to data base;
+    function db_connnect()
+    {
+        global $db;
+        @ $db = new mysqli('localhost', 'user', '1234', 'mydb');
+        global $db_is_connected;
+        if (mysqli_connect_errno())
+        {
+            $db_is_connected = false;
+            alert_message("Error: Could not connect ot database. Please try again later.");
+            return;
+//            exit(alert_message("Error: Could not connect ot database. Please try again later."));
+        }else{
+            $db_is_connected = true;
+            $db->query("SET CHARACTER SET utf8");
+        }
+        return;
+    }
+
+// Insert data in database
+    function db_insert($table, $column, $value)
+    {
+        global $db_is_connected, $db;
+        if (!$db_is_connected)
+        {
+            alert_message("Error: Could not connect to database.");
+            return FALSE;
+//            exit(alert_message("Error: Could not connect to database."));
+        }elseif(count($column) <> count($value))
+        {
+            alert_message("SQL insert query is incorrect");
+            return FALSE;
+        }else
+        {
+            $query = "INSERT INTO $table($column) VALUES ('$value');";
+            $result = $db->query($query);
+            if ($result)
+            {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
+// Delete data from database
+    function db_delete($table, $column, $value)
+{
+    global $db_is_connected, $db;
+    if (!$db_is_connected)
+    {
+        alert_message("Error: Could not connect to database.");
+        return FALSE;
+//        exit(alert_message("Error: Could not connect to database."));
+    }elseif(count($column) <> count($value))
+    {
+        alert_message("SQL delete query is incorrect");
+        return FALSE;
+    }else
+    {
+        $query = "DELETE FROM $table WHERE $column = '$value';";
+        $result = $db->query($query);
+        if ($result)
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+// Select data from database
+    function db_select($table, $column, $group, $ret)
+    {
+        $result = null;
+        global $db_is_connected, $db;
+        if (!$db_is_connected)
+        {
+            alert_message("Error: Could not connect ot database.");
+            return FALSE;
+//            exit(alert_message("Error: Could not connect ot database."));
+        }else
+        {
+            $query = "SELECT $column FROM $table $group;";
+            $rslt = $db->query($query);
+            if (!$rslt)
+            {
+                alert_message($query." isn't correct .");
+                return FALSE;
+            }else{
+                while ($row = $rslt->fetch_assoc()) {
+                    $result[] = $row["$ret"];
+                }
+                return $result;
+            }
+        }
+        return FALSE;
+    }
+		
+?>
