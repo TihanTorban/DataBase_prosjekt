0cac8c150a6f32d87c053bd9d6ffc95de1b6759e
AtharA <aa.tajik@stud.uis.no>
2014-03-27 12:51:51 +0100

    Update2

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 
-echo"<h1>Prosjekt del 3 - Database</h1>";
+echo"<h1>Prosjekt del 3 - Database</h1><br>";
 
-echo"<p>Spørringer: Lag liste over alle eiendeler</p><br>
-Spørringer: Lag liste over eiendeler som organisasjon har lånt med id, beskrivelse, eier, og leveringsdato</p><br>;
-Spørringer: Finn ut hvem som har lånt en bestemt eiendel. List opp navn og kontaktinformasjon</p><br>
-Spørringer: Finn ut hvem man må kontakte for å få tak i en bestemt eiendel/hvem som har adgang til plasseringen eiendelen ligger på</p><br>
-Spørringer: Lag liste over alle eiendeler</p><br><br>
+echo"<h3>Spørringer:</h3>
+- Lag liste over alle eiendeler<br><br>
+- Lag liste over eiendeler som organisasjon har lånt med id, beskrivelse, eier, og leveringsdato<br><br>
+- Finn ut hvem som har lånt en bestemt eiendel. List opp navn og kontaktinformasjon<br><br>
+- Finn ut hvem man må kontakte for å få tak i en bestemt eiendel/hvem som har adgang til plasseringen eiendelen ligger på<br><br>
+- Lag liste over alle eiendeler<br><br><br>
 
-Oppdateringer: <br>
-- Legg inn eiendeler med alle attributter, for alle fremmednøkler skal programmet vise frem en liste, GUI-pull-down meny over alternativer i stedet for et vanlig tekstfelt<br>
+<h3>Oppdateringer: </h3>
+- Legg inn eiendeler med alle attributter, for alle fremmednøkler skal programmet vise frem en liste, GUI-pull-down meny over alternativer i stedet for et vanlig tekstfelt<br><br>
 - Legg inn nye medlemmer eiere og plasseringer";
 
 ?>
